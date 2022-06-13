2ac30eabbb3274841fb9853a9f769705af6f35b6
HeloaGuizzo <107003060+HeloaGuizzo@users.noreply.github.com>
2022-06-13 18:38:30 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 2 Exerc 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------ CÁLCULO DO SALÁRIO -------");
+
+            Console.WriteLine("Digite o número de horas trabalhadas: ");
+            double numeroHoras = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Valor salário-hora: ");
+            double salarioHora = Convert.ToInt32(Console.ReadLine());
+
+            double salario = numeroHoras * salarioHora;
+
+            Console.WriteLine("O salário do funcionário é: R$ " + (salario));
+        }
+    }
+}
